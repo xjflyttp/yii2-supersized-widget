4c1e5f650ff4564d4ab709435561668992984b18
xjflyttp <xjflyttp@gmail.com>
2015-03-06 16:19:46 +0800

    update

--- SupersizedCoreAsset.php
+++ SupersizedCoreAsset.php
@@ -6,25 +6,10 @@ use yii\web\AssetBundle;
 
 class SupersizedCoreAsset extends AssetBundle {
 
-    public $sourcePath;
+    public $sourcePath = '@vendor/xj/yii2-supersized-widget/assets/core';
     public $basePath = '@webroot/assets';
-    public $publishOptions = ['forceCopy' => YII_DEBUG];
     public $css = ['css/supersized.core.css'];
-    public $js = [];
+    public $js = ['js/supersized.core.3.2.1.js'];
     public $depends = ['yii\web\JqueryAsset'];
-
-    private function getJs() {
-        return [
-            YII_DEBUG ? 'js/supersized.core.3.2.1.js' : 'js/supersized.core.3.2.1.min.js',
-        ];
-    }
-
-    public function init() {
-        $this->sourcePath = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'assets/core';
-        if (empty($this->js)) {
-            $this->js = $this->getJs();
-        }
-        return parent::init();
-    }
 
 }
--- SupersizedSlideshowAsset.php
+++ SupersizedSlideshowAsset.php
@@ -6,30 +6,17 @@ use yii\web\AssetBundle;
 
 class SupersizedSlideshowAsset extends AssetBundle {
 
-    public $sourcePath;
+    public $sourcePath = '@vendor/xj/yii2-supersized-widget/assets/slideshow';
     public $basePath = '@webroot/assets';
-    public $publishOptions = ['forceCopy' => YII_DEBUG];
     public $css = [
         'css/supersized.css',
         'theme/supersized.shutter.css',
     ];
-    public $js = [];
+    public $js = [
+        'js/jquery.easing.min.js',
+        'js/supersized.3.2.7.js',
+        'theme/supersized.shutter.js',
+    ];
     public $depends = ['yii\web\JqueryAsset'];
-
-    private function getJs() {
-        return [
-            'js/jquery.easing.min.js',
-            YII_DEBUG ? 'js/supersized.3.2.7.js' : 'js/supersized.3.2.7.min.js',
-            YII_DEBUG ? 'theme/supersized.shutter.js' : 'theme/supersized.shutter.min.js',
-        ];
-    }
-
-    public function init() {
-        $this->sourcePath = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'assets/slideshow';
-        if (empty($this->js)) {
-            $this->js = $this->getJs();
-        }
-        return parent::init();
-    }
 
 }
